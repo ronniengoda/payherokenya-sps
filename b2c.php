a187f7aa0db77009d0835ab7662d3d1a1360b4b5
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-01-10 11:27:59 +0300

    Create b2c.php

---
+++ b2c.php
@@ -0,0 +1,16 @@
+  
+<?php 
+include 'global.php';
+
+//prepare an array that will contain your account details and payment details.
+$data=array(
+        'api_key' =>"your api key here",//provide api keyhere
+        'username'=>"your user name here",//provide username here
+        'amount'=>"10",//provide amount here
+        'phone'=>"phone number here"//provide phone number here
+    );
+  
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/b2c.php",$jdata);
+print_r($response);
+//Thats it hit send and we will take care of the rest
