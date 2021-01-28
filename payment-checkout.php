0a262ae2b21fe85389abe07c6c2baee02123161b
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-01-28 23:58:54 +0300

    Update payment-checkout.php

--- payment-checkout.php
+++ payment-checkout.php
@@ -17,6 +17,8 @@ if (!isset($_POST['phone'])) {
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>PAYMENT CHECKOUT FORM</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
