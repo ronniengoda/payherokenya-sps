fca871e155bc9d65b3e771c207bab0153aa69147
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-02-06 18:11:22 +0300

    Update sample-callback.php

--- sample-callback.php
+++ sample-callback.php
@@ -4,10 +4,11 @@ $response= file_get_contents('php://input');
 $data=json_decode($response);
 
 //Callback response values. You can now use this values for your own good.
-$Transaction_Type=$data->response->Transaction_Type;
-$Source=$data->response->Source;
-$Amount=$data->response->Amount;
-$MPESA_Reference=$data->response->MPESA_Reference;
-$Account=$data->response->Account;
-$User_Reference=$data->response->User_Reference;
-$Transaction_Date=$data->response->Transaction_Date;
+$Transaction_Type=$data->response->Transaction_Type; //can either be C2B or B2C
+$Source=$data->response->Source;//contains customer name and phone for C2B transaction
+$Destination=$data->response->Destination;//contains the receipient of funds name and phone for B2C transaction
+$Amount=$data->response->Amount;//Contains transaction amount
+$MPESA_Reference=$data->response->MPESA_Reference;//Contains MPESA reference number for the transaction
+$Account=$data->response->Account;//Contains your account number;
+$User_Reference=$data->response->User_Reference; //this is the unique payment reference provided for C2B transaction
+$Transaction_Date=$data->response->Transaction_Date;//Contains the date and time when transaction happened
