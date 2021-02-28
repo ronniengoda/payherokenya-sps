8d59488523c6259fc41b1dd7a5eed6768f657e2c
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-02-28 11:18:25 +0300

    Create balance.php
    Use this code to inquire about your account balances.

---
+++ balance.php
@@ -0,0 +1,12 @@
+<?php 
+include 'global.php';
+
+//prepare an array that will contain your account details.Make this request to inquire your wallets balances.
+$data=array(
+        'api_key' =>"",//provide api keyhere
+        'username'=>""//provide username here
+    );
+  
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/balance.php",$jdata);
+print_r($response);
