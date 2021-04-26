a1f9d60d6ebaa7b7099d0a1bc333cd70ce2556c8
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-04-26 17:27:43 +0300

    Create account_transactions.php
    Use this API to fetch all your account transactions

---
+++ account_transactions.php
@@ -0,0 +1,11 @@
+<?php 
+include 'global.php';
+//This API will be used to fetch all your account related transactions.
+$data=array(
+        'api_key' =>"",//provide api keyhere
+        'username'=>""//provide username here
+    );
+  
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/transactions.php",$jdata);
+print_r($response);
