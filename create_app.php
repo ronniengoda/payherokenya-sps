4f8562c5810d01f271494a3d989d9b3ca9821780
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-04-26 15:20:30 +0300

    Create create_app.php
    You can use this API to automatically create an application on our system remotely and get your username, API key, and account number in the response.

---
+++ create_app.php
@@ -0,0 +1,15 @@
+<?php 
+include 'global.php';
+      'username' =>"",//provide your desired username
+        'password'=>"",//provide your password here
+        'email'=>"",//provide your email/business email
+        'business_name'=>"",//provide your business name
+        'registration_number'=>"",//provide your business registration number
+        'contact_phone'=>"",//provide your business contact phone
+    );
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/create_app.php",$jdata);
+print_r($response);
+
+//EXPECTED JSON RESPONSE UPON SUCCESSFULL REGISTRATION
+//{"status":true,"response":{"Status":"Sucess","Message":"Account Created Successfully","Username":"username","Api_Key":"api_key","Paybill":"4060195","Account_Number":"account_number"}}
