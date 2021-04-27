ffd50f747671d949040b7e2549859317ef4ee75c
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-04-27 21:42:13 +0300

    Create topup_service_wallet.php
    Use this api to top up your service wallet from your payments wallet.just provide the amount you want to top up.

---
+++ topup_service_wallet.php
@@ -0,0 +1,12 @@
+<?php 
+include 'global.php';
+//Use this api to top up your service wallet from your payments wallet.just provide the amount you want to top up.
+$data=array(
+        'api_key' =>"",//provide api keyhere
+        'username'=>"",//provide username here
+        'amount'=>20//provide amount
+    );
+  
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/topup_service_wallet.php",$jdata);
+print_r($response);
