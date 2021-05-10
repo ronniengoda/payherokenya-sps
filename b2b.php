a938f2c6fe8c2e83113b73b7bfd9916c6b7b2d83
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-05-10 19:05:15 +0300

    Create b2b.php

---
+++ b2b.php
@@ -0,0 +1,14 @@
+<?php 
+include 'global.php';
+//Use this API to transfer funds from your payments wallet to another account's payments wallet. Provide the details required to complete transaction
+$data=array(
+        'api_key' =>"",//provide api keyhere
+        'username'=>"",//provide username here
+        'amount'=>"",//provide amount
+        'destination_account_number'=>'',//provide destination/recipient account number.
+        'payment_reference'=>'NA'//provide the payment reference for this transaction,could be anything unique to identify the transaction eg: invoice number etc.
+    );
+  
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/b2b.php",$jdata);
+print_r($response);
