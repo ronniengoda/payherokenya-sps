705e03aed89306ab0c759124486fa542a2b9fe1e
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-05-10 13:21:28 +0300

    Update sample-callback.php

--- sample-callback.php
+++ sample-callback.php
@@ -10,7 +10,8 @@ $Destination=$data->response->Destination;//contains the receipient of funds nam
 $Amount=$data->response->Amount;//Contains transaction amount
 $MPESA_Reference=$data->response->MPESA_Reference;//Contains MPESA reference number for the transaction
 $Account=$data->response->Account;//Contains your account number;
-$User_Reference=$data->response->User_Reference; //this is the unique payment reference provided for C2B transaction
+$User_Reference=$data->response->User_Reference; //this is the unique payment reference provided for C2B transaction type.
+$Transaction_Reference=$data->response->Transaction_Reference;//this is the unique transaction reference for B2C transaction type.
 $Transaction_Date=$data->response->Transaction_Date;//Contains the date and time when transaction happened
 
 #You can now write your logic to process the above below here.
