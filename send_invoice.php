c5891e912d3f025b5090403617d8428dcf9360a1
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-06-22 14:36:31 +0300

    Create send_invoice.php

---
+++ send_invoice.php
@@ -0,0 +1,17 @@
+<?php 
+include 'global.php';
+//Use this API to send your customers invoices,provide the required parameters and an email will be sent to your customer containing the invoice details and a link to make the payment.
+$data=array(
+        'api_key' =>"",//provide api keyhere
+        'username'=>"",//provide username here
+        'amount'=>"",//provide the amount to be paid
+        'invoice_number'=>"",//provide the invoice number: this will be sent to your callback as user reference.
+        'description'=>"",//provide a brief decription of this invoice.
+        'customer_email'=>"",//provide customer email
+        'customer_phone'=>"",//provide customer phone
+        'customer_name'=>""//provide customer name.
+    );
+  
+$jdata=json_encode($data);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/send_invoice.php",$jdata);
+print_r($response);
