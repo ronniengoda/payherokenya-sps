85ff52dbd3a47eb8174b7575949e0d8c0e4bf7d3
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2021-06-29 12:30:38 +0300

    Update register_callback.php

--- register_callback.php
+++ register_callback.php
@@ -7,5 +7,5 @@ $data=array(
         'callback_url'=>"",//provide your desired callback url should start with https://
     );
 $jdata=json_encode($data);
-$response=sendRequest("https://payherokenya.com/sps/portal/app/register_callback.php",$jdata);
+$response=sendRequest("https://payherokenya.com/sps/portal/app/register_callback",$jdata);
 print_r($response);
